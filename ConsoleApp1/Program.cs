1597c50dcdc025dd2cd3aab544cf786a8c96ff35
NANAME002 <dennzerogo@gmail.com>
2024-11-25 21:18:05 +0200

    add1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"x={Func(5) + Func(12) + Func(19):F2}");
+            Console.ReadLine();
+        }
+        public static double Func(double x)
+        {
+            return (Math.Sqrt(x) + x) / 2;
+        }
+    }
+}
